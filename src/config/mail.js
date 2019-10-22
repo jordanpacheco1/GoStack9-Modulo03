8476a954a6ecf566e9137f54509a70f01490a1ea
Jordan <jordanrune@gmail.com>
2019-10-22 15:52:17 -0300

    Modulo 03 GoStack9

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: '2525',
+  secure: false,
+  auth: {
+    user: '0823e55e25ee5d',
+    pass: '49f7a02e09f6d7',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
